ea2578b90da5c657f4e0bdf89d0fdf4966403afd
Marius-Juston <marius.juston@hotmail.fr>
2021-01-31 16:45:28 -0500

    simplified live range plotter using live_plotter

--- src/range_drawer.py
+++ src/range_drawer.py
@@ -6,17 +6,13 @@ matplotlib.use('Qt5Agg')
 import rospy
 import matplotlib.pyplot as plt
 from gtec_msgs.msg import Ranging
-
+from live_plotter import LivePlotter
 
 class RangePlotter:
     def __init__(self, tag_ids=("0", "1"), n=-1):
-        self.data = dict()
-
         toa_ranging = '/gtec/toa/ranging'
 
-        self.axes = plt.gca()
-        self.axes.legend()
-        self.colors = ['r', 'b', 'g']
+        self.live_plotter = LivePlotter()
 
         self.n = n
 
@@ -32,58 +28,15 @@ class RangePlotter:
 
         label = "tag{}_anchor{}".format(tag_id, anchor_id)
 
-        if label not in self.data:
-
-            line = self.axes.plot([], [], label=label)[0]
-
-            self.data[label] = (
-                [],
-                [],
-                line
-            )
-
         time = rospy.Time.now().to_sec()
 
-        self.data[label][0].append(msg.range)
-        self.data[label][1].append(time)
-
-        self.data[label][2].set_xdata(self.data[label][1])
-        self.data[label][2].set_ydata(self.data[label][0])
+        self.live_plotter.add_data_point(label, time, msg.range)
 
     def run(self):
-
-        rate = rospy.Rate(50)
-
-
-        while not rospy.is_shutdown():
-            self.axes.legend()
-            # print(self.data)
-            self.axes.relim()      # make sure all the data fits
-            self.axes.autoscale()  # auto-scale
-
-            if self.n > 0 :
-                x_max = self.axes.get_xlim()[-1]
-                x_min = max(x_max - self.n, self.axes.get_xlim()[0])
-
-                self.axes.set_xlim([x_min, x_max])
-
-            plt.draw()
-            plt.pause(0.0001)
-            rate.sleep()
+        self.live_plotter.show()
         
 if __name__ == "__main__":
     rospy.init_node("data_drawer_node")
 
-    every = matplotlib.rcsetup.all_backends
-
-    for name in every:
-        out = matplotlib.rcsetup.validate_backend(name)
-        print out, name
-
-    plt.ion()
-    plt.show()
-
     data_plotter = RangePlotter()
     data_plotter.run()
-
-    plt.show()
