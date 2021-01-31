adec20678d0e4ad12bed7b57d9984314d3aa4601
Marius-Juston <marius.juston@hotmail.fr>
2021-01-31 15:44:57 -0500

    publish the current robot position

---
+++ src/actual_robot_position.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import rospy
+import tf
+from geometry_msgs.msg import Point
+
+import sys
+
+if __name__ == "__main__":
+    rospy.init_node("robot_position_publisher_node")
+
+    if sys.argv < 3:
+        raise ValueError("Expecting arugments: world_link robot_base_link")
+
+    else:
+        world_link, robot_base_link = sys.argv[1], sys.argv[2]
+
+    name = "/data_drawer/robot_pose"
+
+    pub = rospy.Publisher(name, Point, queue_size=1)
+    tf_listener = tf.TransformListener()
+
+    rate = rospy.Rate(100)
+
+    point = Point()
+
+    while not rospy.is_shutdown():
+        try:
+            (trans,rot) = self.tf_listener.lookupTransform(self.world_link, self.robot_link, rospy.Time(0))
+
+            point.x, point.y = trans[0], trans[1]
+
+            pub.publish(point)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            print "Unable to find transform"
+
+        rate.sleep()
