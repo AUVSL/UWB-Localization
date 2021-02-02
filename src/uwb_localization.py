712fb928915d54420d77af5d61a96a0408733437
Marius-Juston <marius.juston@hotmail.fr>
2021-02-02 17:40:57 -0500

    added second tag kalman filter

--- src/uwb_localization.py
+++ src/uwb_localization.py
@@ -9,7 +9,8 @@ from nav_msgs.msg import Odometry
 
 class UWBLocalization:
     def __init__(self):
-        self.kalman_filter = FusionEKF()
+        self.kalman_filter_tag_0 = FusionEKF()
+        self.kalman_filter_tag_1 = FusionEKF()
 
         self.anchor_poses = dict()
 
@@ -19,9 +20,12 @@ class UWBLocalization:
         anchors_sub = rospy.Subscriber(anchors, MarkerArray, callback=self.add_anchors)
         ranging_sub = rospy.Subscriber(toa_ranging, Ranging, callback=self.add_ranging)
 
-        publish = '/jackal/uwb/pose'
+        publish_tag_0 = '/jackal/uwb/pose/0'
+        publish_tag_1 = '/jackal/uwb/pose/1'
 
-        self.estimated_pose = rospy.Publisher(publish, Odometry, queue_size=1)
+        self.estimated_pose_tag_0 = rospy.Publisher(publish_tag_0, Odometry, queue_size=1)
+        self.estimated_pose_tag_1 = rospy.Publisher(publish_tag_1, Odometry, queue_size=1)
+        
         self.pose = Odometry()
 
     def add_anchors(self, msg):
@@ -33,26 +37,38 @@ class UWBLocalization:
     def add_ranging(self, msg):
         # type: (Ranging) -> None
 
-        if msg.tagId == 0:
-            if msg.anchorId in self.anchor_poses:
-                anchor_pose = self.anchor_poses[msg.anchorId]
-                anchor_distance = msg.range / 1000.
+        if msg.anchorId in self.anchor_poses:
+            anchor_pose = self.anchor_poses[msg.anchorId]
+            anchor_distance = msg.range / 1000.
 
-                self.kalman_filter.process_measurement(anchor_pose, anchor_distance)
+            if msg.tagId == 0:
+                self.kalman_filter_tag_0.process_measurement(anchor_pose, anchor_distance)
+            elif msg.tagId == 1:
+                self.kalman_filter_tag_1.process_measurement(anchor_pose, anchor_distance)
 
     def run(self):
         rate = rospy.Rate(60)
 
         while not rospy.is_shutdown():
-            self.pose.pose.pose.position.x = self.kalman_filter.kalman_filter.x[0]
-            self.pose.pose.pose.position.y = self.kalman_filter.kalman_filter.x[1]
-            self.pose.pose.pose.position.z = self.kalman_filter.kalman_filter.x[2]
+            self.pose.pose.pose.position.x = self.kalman_filter_tag_0.kalman_filter.x[0]
+            self.pose.pose.pose.position.y = self.kalman_filter_tag_0.kalman_filter.x[1]
+            self.pose.pose.pose.position.z = self.kalman_filter_tag_0.kalman_filter.x[2]
 
-            self.pose.twist.twist.linear.x = self.kalman_filter.kalman_filter.x[3]
-            self.pose.twist.twist.linear.y = self.kalman_filter.kalman_filter.x[4]
-            self.pose.twist.twist.linear.z = self.kalman_filter.kalman_filter.x[5]
+            self.pose.twist.twist.linear.x = self.kalman_filter_tag_0.kalman_filter.x[3]
+            self.pose.twist.twist.linear.y = self.kalman_filter_tag_0.kalman_filter.x[4]
+            self.pose.twist.twist.linear.z = self.kalman_filter_tag_0.kalman_filter.x[5]
 
-            self.estimated_pose.publish(self.pose)
+            self.estimated_pose_tag_0.publish(self.pose)
+            
+            self.pose.pose.pose.position.x = self.kalman_filter_tag_1.kalman_filter.x[0]
+            self.pose.pose.pose.position.y = self.kalman_filter_tag_1.kalman_filter.x[1]
+            self.pose.pose.pose.position.z = self.kalman_filter_tag_1.kalman_filter.x[2]
+
+            self.pose.twist.twist.linear.x = self.kalman_filter_tag_1.kalman_filter.x[3]
+            self.pose.twist.twist.linear.y = self.kalman_filter_tag_1.kalman_filter.x[4]
+            self.pose.twist.twist.linear.z = self.kalman_filter_tag_1.kalman_filter.x[5]
+
+            self.estimated_pose_tag_1.publish(self.pose)
 
             rate.sleep()
 
