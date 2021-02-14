6d13e2d9bbc227eae805d6525604330b031488ef
Marius-Juston <marius.juston@hotmail.fr>
2021-02-14 18:32:00 -0500

    removed predicted sigma angle check

--- src/ukf/state_predictor.py
+++ src/ukf/state_predictor.py
@@ -92,8 +92,8 @@ class StatePredictor:
 
         # ------------------
 
-        predicted_sigma[3,predicted_sigma[3] < 0] += (2 * np.pi)
-        predicted_sigma[3, predicted_sigma[3] > np.pi] %= (2 * np.pi)
+        # predicted_sigma[3,predicted_sigma[3] < 0] += (2 * np.pi)
+        # predicted_sigma[3, predicted_sigma[3] > np.pi] %= (2 * np.pi)
 
         return predicted_sigma
 
