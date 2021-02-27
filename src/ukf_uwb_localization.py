e0f2ade68d8119a4ed9c8843cadd10e96275a456
Marius-Juston <mjuston2@illinois.edu>
2021-02-26 21:44:32 -0500

    implemented initial uwb localization  using least square

--- src/ukf_uwb_localization.py
+++ src/ukf_uwb_localization.py
@@ -9,6 +9,7 @@ from visualization_msgs.msg import MarkerArray
 from gtec_msgs.msg import Ranging
 import tf
 from tf.transformations import euler_from_quaternion, euler_from_quaternion 
+from scipy.optimize import least_squares
 
 class UKFUWBLocalization:
     def __init__(self, uwb_std=1, odometry_std=(1,1,1,1,1,1), accel_std=1, yaw_accel_std=1, alpha=1, beta=0):
@@ -26,11 +27,14 @@ class UKFUWBLocalization:
         self.ukf = FusionUKF(sensor_std, accel_std, yaw_accel_std, alpha, beta)
 
         self.anchor_poses = dict()
-        # self.tag_offset = self.retrieve_tag_offsets({"left_tag":1, "right_tag":0})
-        self.tag_offset = {
-            1:np.array([0, 0.162, 0.184]),
-            0:np.array([0, -0.162, 0.184])
-        }
+        self.tag_offset = self.retrieve_tag_offsets({"left_tag":1, "right_tag":0})
+
+        # right: 0
+        # left: 1
+        # self.tag_offset = {
+        #     0:np.array([0, -0.162, 0.184]),
+        #     1:np.array([0, 0.162, 0.184])
+        # }
         # print(self.tag_offset)
 
         anchors = '/gtec/toa/anchors'
@@ -46,6 +50,10 @@ class UKFUWBLocalization:
         self.odom = Odometry()
 
         self.sensor_data = []
+
+        self.initialized = False
+        self.start_translation = np.zeros(2)
+        self.start_rotation = 0
 
     def retrieve_tag_offsets(self, tags, base_link='base_link'):
         transforms = dict() 
@@ -99,6 +107,10 @@ class UKFUWBLocalization:
 
         theta_yaw = msg.twist.twist.angular.z
 
+        theta_yaw += self.start_rotation
+        px += self.start_translation[0]
+        py += self.start_translation[1]
+
         data = DataPoint(DataType.ODOMETRY, np.array([px, py, pz, v, theta, theta_yaw]), t)
 
         self.sensor_data.append(data)
@@ -132,8 +144,12 @@ class UKFUWBLocalization:
         t = self.get_time()
 
         self.ukf.initialize(x, P, t)
+        self.initialized = True
 
-    def run(self):
+    def run(self, initial_P=None):
+        if not self.initialized:
+            self.initialize_pose(initial_P)
+
         rate = rospy.Rate(60)
 
         while not rospy.is_shutdown():
@@ -159,6 +175,81 @@ class UKFUWBLocalization:
 
             rate.sleep()
 
+    def func(self, x, d, distances):
+        # x[0] = left_x
+        # x[1] = left_y
+        # x[2] = right_x
+        # x[3] = right_y
+
+        x1, y1, x2, y2 = x
+
+        residuals = [
+            (x1 - x2) ** 2 + (y1 - y2) ** 2 - d ** 2,
+        ]
+
+        for distance in distances:
+            anchor = distance['anchor']
+            tag = distance['tag']
+            distance = distance['dist']
+
+            z = tag[2]
+
+            if np.all(tag == self.tag_offset[1]):
+                x = x1
+                y = y1
+            else:
+                x = x2
+                y = y2
+
+            residuals.append((x - anchor[0]) ** 2 + (y - anchor[1]) ** 2 + (z - anchor[2]) ** 2 - distance ** 2)
+
+        return residuals
+
+    def initialize_pose(self, initial_P=None):
+
+        delay = 1 #s
+        rate = rospy.Rate(delay)
+
+        d = np.linalg.norm(self.tag_offset[0] - self.tag_offset[1])
+
+        while not rospy.is_shutdown() and not self.initialized:
+            rate.sleep()
+
+            uwb = []
+
+            for s in self.sensor_data:
+                if s.data_type == DataType.UWB:
+                    uwb.append({
+                        'anchor': s.extra['anchor'],
+                        'tag': s.extra['sensor_offset'],
+                        'dist': s.measurement_data
+                    })
+
+            if len(uwb) > 30:
+                res = least_squares(self.func, [0,0,0,0], args=(d, uwb))
+
+                # print(res)
+
+                left = res.x[0:2]
+                right = res.x[2:4]
+                
+                center = (left + right) / 2
+                v_ab = left - right
+                theta = np.arccos(v_ab[1] / np.linalg.norm(v_ab))
+
+                print(center, v_ab, theta, np.degrees(theta))
+
+                del self.sensor_data[:]
+
+                self.start_translation = center
+                self.start_rotation = theta
+
+                if initial_P is None:
+                    initial_P = np.identity(6)
+
+                self.intialize(np.array([center[0], center[1], 0, 0, theta, 0 ]), initial_P)
+
+
 if __name__ == "__main__":
     rospy.init_node("ukf_uwb_localization_kalman")
 
@@ -174,11 +265,12 @@ if __name__ == "__main__":
 
     print x, y, v, theta
 
-    p = [7.0001, 11.0, 14.0001, 20.9001, 1.0001, 0.0001, 0.0001, 3.9001, 4.9001, 1.0, 0, 0.0001, 0.0001, 0.0001, 2.0001, 0.0001, 0.0001]
+    p = [1.0001, 11.0, 14.0001, 20.9001, 1.0001, 0.0001, 0.0001, 3.9001, 4.9001, 1.0, 0, 0.0001, 0.0001, 0.0001, 2.0001, 0.0001, 0.0001]
 
 
     loc = UKFUWBLocalization(p[0], p[1:7], accel_std=p[7], yaw_accel_std=p[8], alpha=p[9], beta=p[10])
-    loc.intialize(np.array([x, y, z, v, theta ]), np.diag(p[11:17]))
+    # loc.intialize(np.array([x, y, z, v, theta ]),
+        # np.identity(6))
 
     loc.run()
 
