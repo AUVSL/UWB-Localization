fixed covariance weight values

src/ukf/fusion_ukf.py
@@ -7,7 +7,7 @@ from ukf.state_updater import StateUpdater
 
 
 class FusionUKF(object):
-    def __init__(self, sensor_std, speed_noise_std=.9, yaw_rate_noise_std=.6, alpha=1, beta=0):
+    def __init__(self, sensor_std, speed_noise_std=.9, yaw_rate_noise_std=.6, alpha=1, beta=0, k=None):
         # ODOMETRY: beta=0.3
         # UWB: beta=0.3
 
@@ -17,17 +17,28 @@ class FusionUKF(object):
         # Number of total states X, Y, Z, velocity, yaw, yaw rate
         self.NX = 6
 
+
         # Settings values -----------------------------------
         self.N_AUGMENTED = self.NX + 2
+
+        if k is None:
+            self.K = 3 - self.N_AUGMENTED
+        else:
+            self.K = k
+
         self.N_SIGMA = self.N_AUGMENTED * 2 + 1
         self.ALPHA = alpha
-        self.LAMBDA = (self.ALPHA ** 2) * (self.NX + 3 - self.N_AUGMENTED) - self.NX
+        self.LAMBDA = (self.ALPHA ** 2) * (self.NX + self.K) - self.NX
         self.SCALE = np.sqrt(self.LAMBDA + self.N_AUGMENTED)
         self.W = 0.5 / (self.LAMBDA + self.N_AUGMENTED)
-        self.W0 = self.LAMBDA / (self.LAMBDA + self.N_AUGMENTED) + (1 - alpha ** 2 + beta)
+        self.W0_M = self.LAMBDA / (self.LAMBDA + self.N_AUGMENTED)
+        self.W0_C = self.LAMBDA / (self.LAMBDA + self.N_AUGMENTED) + (1 - alpha ** 2 + beta)
 
-        self.WEIGHTS = np.full(self.N_SIGMA, self.W)
-        self.WEIGHTS[0] = self.W0
+        self.WEIGHTS_M = np.full(self.N_SIGMA, self.W)
+        self.WEIGHTS_M[0] = self.W0_M
+
+        self.WEIGHTS_C = np.full(self.N_SIGMA, self.W)
+        self.WEIGHTS_C[0] = self.W0_C
         # -----------------------------------
 
         # Uncertainty Settings -----------------------------------
@@ -51,11 +62,11 @@ class FusionUKF(object):
         self.nis = 0
 
         self.state_predictor = StatePredictor(self.NX, self.N_SIGMA, self.N_AUGMENTED, self.SPEED_NOISE_VAR,
-                                              self.YAW_RATE_NOISE_VAR, self.SCALE, self.WEIGHTS)
+                                              self.YAW_RATE_NOISE_VAR, self.SCALE, self.WEIGHTS_M, self.WEIGHTS_C)
 
-        self.measurement_predictor = MeasurementPredictor(sensor_std, self.N_SIGMA, self.WEIGHTS)
+        self.measurement_predictor = MeasurementPredictor(sensor_std, self.N_SIGMA, self.WEIGHTS_M, self.WEIGHTS_C)
 
-        self.state_updater = StateUpdater(self.NX, self.N_SIGMA, self.WEIGHTS)
+        self.state_updater = StateUpdater(self.NX, self.N_SIGMA, self.WEIGHTS_M, self.WEIGHTS_C)
 
     def initialize(self, x, initial_p, timestamp):
         self.x[:x.size] = x

src/ukf/measurement_predictor.py
@@ -7,12 +7,13 @@ from ukf.util import normalize
 
 
 class MeasurementPredictor(object):
-    def __init__(self, sensor_std, N_SIGMA, WEIGHTS):
+    def __init__(self, sensor_std, N_SIGMA, WEIGHTS_M, WEIGHTS_C):
+        self.WEIGHTS_C = WEIGHTS_C
         self.sensor_std = sensor_std
 
         self.compute_R_matrix()
 
-        self.WEIGHTS = WEIGHTS
+        self.WEIGHTS_M = WEIGHTS_M
         self.N_SIGMA = N_SIGMA
 
         self.z = None
@@ -76,7 +77,7 @@ class MeasurementPredictor(object):
         return sigma
 
     def compute_z(self, sigma):
-        return np.dot(sigma, self.WEIGHTS)
+        return np.dot(sigma, self.WEIGHTS_M)
 
     def compute_S(self, sigma, z):
         sub = np.subtract(sigma.T, z).T
@@ -84,7 +85,7 @@ class MeasurementPredictor(object):
         if self.current_type == DataType.ODOMETRY:
             normalize(sub, UKFState.YAW)
 
-        return (np.matmul(self.WEIGHTS * sub, sub.T)) + self.R
+        return (np.matmul(self.WEIGHTS_C * sub, sub.T)) + self.R
 
     def process(self, sigma_x, data):
         self.initialize(data)

src/ukf/state_predictor.py
@@ -6,8 +6,9 @@ from ukf.util import normalize
 
 
 class StatePredictor(object):
-    def __init__(self, NX, N_SIGMA, N_AUGMENTED, VAR_SPEED_NOISE, VAR_YAW_RATE_NOISE, SCALE, WEIGHTS):
-        self.WEIGHTS = WEIGHTS
+    def __init__(self, NX, N_SIGMA, N_AUGMENTED, VAR_SPEED_NOISE, VAR_YAW_RATE_NOISE, SCALE, WEIGHTS_M, WEIGHTS_C):
+        self.WEIGHTS_M = WEIGHTS_M
+        self.WEIGHTS_C = WEIGHTS_C
         self.NX = NX
         self.SCALE = SCALE
         self.N_SIGMA = N_SIGMA
@@ -101,14 +102,14 @@ class StatePredictor(object):
         return predicted_sigma
 
     def predict_x(self, predicted_sigma):
-        return np.dot(predicted_sigma, self.WEIGHTS)
+        return np.dot(predicted_sigma, self.WEIGHTS_M)
 
     def predict_P(self, predicted_sigma, predicted_x):
         sub = np.subtract(predicted_sigma.T, predicted_x).T
 
         normalize(sub, UKFState.YAW)
 
-        return np.matmul(self.WEIGHTS * sub, sub.T)
+        return np.matmul(self.WEIGHTS_C * sub, sub.T)
 
     def process(self, x, P, dt):
         augmented_sigma = self.compute_augmented_sigma(x, P)

src/ukf/state_updater.py
@@ -7,9 +7,10 @@ from ukf.util import normalize
 
 
 class StateUpdater(object):
-    def __init__(self, NX, N_SIGMA, WEIGHTS):
+    def __init__(self, NX, N_SIGMA, WEIGHTS_M, WEIGHTS_C):
+        self.WEIGHTS_C = WEIGHTS_C
         self.N_SIGMA = N_SIGMA
-        self.WEIGHTS = WEIGHTS
+        self.WEIGHTS_M = WEIGHTS_M
         self.NX = NX
         self.x = None
         self.P = None
@@ -24,7 +25,7 @@ class StateUpdater(object):
 
         normalize(dz, UKFState.YAW)
 
-        return np.matmul(self.WEIGHTS * dx, dz)
+        return np.matmul(self.WEIGHTS_C * dx, dz)
 
     def update(self, z, S, Tc, predicted_z, predicted_x, predicted_P, data_type):
         Si = np.linalg.inv(S)