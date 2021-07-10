47f6022895df0710223ad5a0c300b06cbc434a43
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-10 19:20:38 -0400

    Commented the util functions

--- src/ukf/util.py
+++ src/ukf/util.py
@@ -3,11 +3,22 @@ import numpy as np
 
 
 def normalize(d, index):
+    """
+    In place normalizes angle values to be between -pi to pi, at a specific index at array
+    @param d: The array to inplace normalize the angle values
+    @param index: the index that the angle values are located
+    """
     d[index] = (d[index] + np.pi) % (2 * np.pi) - np.pi
 
 
-def angle_diff(angle1, angle2):
-    diff = (angle2 - angle1 + np.pi) % (2 * np.pi) - np.pi
+def angle_diff(start, end):
+    """
+    Calculates the bounded angle difference between the start and end value
+    @param start: the starting angle, in radians
+    @param end: the ending angle, in radians
+    @return: the bounded angle difference between -180 and 180, in radians
+    """
+    diff = (end - start + np.pi) % (2 * np.pi) - np.pi
 
     diff[diff < -np.pi] += 2 * np.pi
 
