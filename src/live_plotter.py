a1685da0f62ba117adb5a5b799c71fb566e1ca8d
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-10 17:41:13 -0400

    commented the live plotter class

--- src/live_plotter.py
+++ src/live_plotter.py
@@ -6,7 +6,20 @@ from matplotlib.animation import FuncAnimation
 
 
 class LivePlotter(object):
+    """
+    A helper class for creating live plotting matplotlib windows
+    """
+
     def __init__(self, update_interval=1000, alpha=1.0, window_name=None, time_removal=None, lengend_loc='best', font_size=None):
+        """
+        Setups the live plotting window
+        @param update_interval: How fast the animated window updates its values
+        @param alpha: The alpha value of lines added if there are many to make things a little easier to view
+        @param window_name: The matplotlib window name
+        @param time_removal:  How long before you want to remove the legend, None to keep it indefinitely
+        @param lengend_loc: The location of the legend as defined by matplotlib parameter choices
+        @param font_size: The font size of the legend as defined by matplotlib parameter choices
+        """
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(1, 1, 1)
         self.fig.tight_layout()
@@ -24,6 +37,13 @@ class LivePlotter(object):
         self.font_size = font_size
 
     def add_data_point(self, object_name, x, y):
+        """
+        Adds data to the live plotting window
+        @param object_name: The line object to add the data to. If it does not exist a new object will be created
+        otherwise the data will just be appended to
+        @param x: the x data to add
+        @param y: the y data to add
+        """
         if object_name not in self.data:
             self.data[object_name] = {
                 "x": [],
@@ -41,6 +61,11 @@ class LivePlotter(object):
         self.objects[object_name].set_ydata(self.data[object_name]['y'])
 
     def func_animate(self, i):
+        """
+        The animation function called at each interval step
+        @param i: the current index of the animation
+        @return: the objects to draw
+        """
         try:
             if self.time_removal is None or i < self.time_removal:
                 # if self.time_removal is not None:
@@ -57,6 +82,9 @@ class LivePlotter(object):
         return self.objects.values()
 
     def show(self):
+        """
+        Shows and starts the live plotter func loop
+        """
         self.ani = FuncAnimation(self.fig, self.func_animate, interval=self.update_interval)
 
         plt.show()
