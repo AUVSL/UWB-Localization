2f47e82618cdfa765180df241d4f26a0ff506808
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-10 18:44:34 -0400

    Created angle diff function bound by -pi and pi

--- src/ukf/util.py
+++ src/ukf/util.py
@@ -5,6 +5,14 @@ import numpy as np
 def normalize(d, index):
     d[index] = (d[index] + np.pi) % (2 * np.pi) - np.pi
 
+
+def angle_diff(angle1, angle2):
+    diff = (angle2 - angle1 + np.pi) % (2 * np.pi) - np.pi
+
+    diff[diff < -np.pi] += 2 * np.pi
+
+    return diff
+
     # d[index] %= 2 * np.pi
     # mask = np.abs(d[index]) > np.pi
     # d[index, mask] -= (np.pi * 2)
