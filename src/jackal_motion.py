64628e84ee3944a3611588a21da956bff75c5268
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-10 17:33:22 -0400

    Commeneted JackalMotion class

--- src/jackal_motion.py
+++ src/jackal_motion.py
@@ -6,7 +6,14 @@ from geometry_msgs.msg import Twist
 
 
 class JackalMotion(object):
+    """
+    Class meant to publish to the Jackal motion commands
+    """
     def __init__(self, namespace):
+        """
+        Setups publisher helper. Publishes to <namespace>jackal_velocity_controller/cmd_vel
+        @param namespace: the robot you want to publish to .i.e Jackal1/
+        """
         self.v = 0
         self.w = 0
 
@@ -15,6 +22,9 @@ class JackalMotion(object):
         self.vel_pub = rospy.Publisher(namespace + 'jackal_velocity_controller/cmd_vel', Twist, queue_size=1)
 
     def step(self):
+        """
+        Step function that publishes the velocity commands
+        """
         self.cmd.linear.x = self.v
         self.cmd.angular.z = self.w
 
