f1309c2ef4909e94c152a97b72c7ebd60e8ae660
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-10 18:53:37 -0400

    in state updater, for measurement difference used angle difference for yaw

--- src/ukf/state_updater.py
+++ src/ukf/state_updater.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from ukf.datapoint import DataType
 from ukf.state import UKFState
-from ukf.util import normalize
+from ukf.util import normalize, angle_diff
 
 
 class StateUpdater(object):
@@ -16,14 +16,19 @@ class StateUpdater(object):
         self.P = None
         self.nis = None
 
-    def compute_Tc(self, predicted_x, predicted_z, sigma_x, sigma_z):
+    def compute_Tc(self, predicted_x, predicted_z, sigma_x, sigma_z, data_type):
         dx = np.subtract(sigma_x.T, predicted_x).T
 
-        normalize(dx, UKFState.YAW)
+        # normalize(dx, UKFState.YAW)
 
         dz = np.subtract(sigma_z.T, predicted_z)
 
-        normalize(dz, UKFState.YAW)
+        if data_type == DataType.ODOMETRY:
+            dz[:, UKFState.YAW] = angle_diff(sigma_z[UKFState.YAW], predicted_z[UKFState.YAW])
+        elif data_type == DataType.IMU:
+            dz[:, 0] = angle_diff(sigma_z[0], predicted_z[0])
+
+        # normalize(dz, UKFState.YAW)
 
         return np.matmul(self.WEIGHTS_C * dx, dz)
 
@@ -34,9 +39,11 @@ class StateUpdater(object):
         dz = z - predicted_z
 
         if data_type == DataType.ODOMETRY:
-            normalize(dz, UKFState.YAW)
+            dz[UKFState.YAW] = angle_diff(np.atleast_1d(z[UKFState.YAW]), np.atleast_1d(predicted_z[UKFState.YAW]))
+        elif data_type == DataType.IMU:
+            dz[0] = angle_diff(np.atleast_1d(z[0]), np.atleast_1d(predicted_z[0]))
 
-            # print(z[UKFState.YAW], predicted_z[UKFState.YAW], dz[UKFState.YAW])
+        # print(z[UKFState.YAW], predicted_z[UKFState.YAW], dz[UKFState.YAW])
 
         # Dm = np.sqrt(np.matmul(np.matmul(dz, Si), dz))
 
@@ -45,7 +52,7 @@ class StateUpdater(object):
         self.nis = np.matmul(dz.transpose(), np.matmul(Si, dz))
 
     def process(self, predicted_x, predicted_z, z, S, predicted_P, sigma_x, sigma_z, data_type):
-        Tc = self.compute_Tc(predicted_x, predicted_z, sigma_x, sigma_z)
+        Tc = self.compute_Tc(predicted_x, predicted_z, sigma_x, sigma_z, data_type)
         self.update(z, S, Tc, predicted_z, predicted_x, predicted_P, data_type)
 
         normalize(self.x, UKFState.YAW)
