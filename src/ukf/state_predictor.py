Added correct angle normalization

src/ukf/state_predictor.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 from ukf.state import UKFState
+from ukf.util import angle_diff
 
 
 class StatePredictor(object):
@@ -106,7 +107,7 @@ class StatePredictor(object):
     def predict_P(self, predicted_sigma, predicted_x):
         sub = np.subtract(predicted_sigma.T, predicted_x).T
 
-        # normalize(sub, UKFState.YAW)
+        sub[UKFState.YAW] = angle_diff(predicted_sigma[UKFState.YAW], predicted_x[UKFState.YAW])
 
         return np.matmul(self.WEIGHTS_C * sub, sub.T)
 