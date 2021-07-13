fd7c3aa4fce9c2980987c62763faf72246563f51
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-12 21:14:46 -0400

    commented the state predictor

--- src/ukf/state_predictor.py
+++ src/ukf/state_predictor.py
@@ -6,7 +6,23 @@ from ukf.util import angle_diff
 
 
 class StatePredictor(object):
+    """
+    A class that handles the state prediction step of the UKF following a CTRV model
+    """
+
     def __init__(self, NX, N_SIGMA, N_AUGMENTED, VAR_SPEED_NOISE, VAR_YAW_RATE_NOISE, SCALE, WEIGHTS_M, WEIGHTS_C):
+        """
+        Setups the State Predictor class
+        @param NX: the number of states, 6 for the CTRV model. x, y, z, v, theta, theta rate
+        @param N_SIGMA: the number of sigma points to generate
+        @param N_AUGMENTED: the number of augmented states, 8 for the CTRV model. x, y, z, v, theta, theta rate,
+        linear acceleration, yaw rate acceleration
+        @param VAR_SPEED_NOISE: the expect linear acceleration defined as a Gaussian distribution
+        @param VAR_YAW_RATE_NOISE: the expected angular acceleration defined as a Gaussian distribution
+        @param SCALE: the sigma point scale
+        @param WEIGHTS_M: the weights for the state calculation
+        @param WEIGHTS_C: the weights for the covariance matrix calculation
+        """
         self.WEIGHTS_M = WEIGHTS_M
         self.WEIGHTS_C = WEIGHTS_C
         self.NX = NX
@@ -23,6 +39,12 @@ class StatePredictor(object):
         self.YAW_RATE_THRESHOLD = 0.001
 
     def compute_augmented_sigma(self, x, P):
+        """
+        Creates the augmented sigma point matrix
+        @param x: the current state vector
+        @param P: the current covariance matrix
+        @return: a matrix with the size N_AUGMENTED x N_SIGMA_POINTS
+        """
         augmented_x = np.zeros(self.N_AUGMENTED)
         augmented_P = np.zeros((self.N_AUGMENTED, self.N_AUGMENTED))
 
@@ -42,6 +64,12 @@ class StatePredictor(object):
         return augmented_sigma
 
     def predict_sigma(self, augmented_sigma, dt):
+        """
+        Coverts the current augmented sigma matrix to the next predicted state given dt time.
+        @param augmented_sigma: the current sigma point state matrix
+        @param dt: the time that has elapsed
+        @return: the sigma point matrix given dt time has passed
+        """
         predicted_sigma = np.zeros((self.NX, self.N_SIGMA))
 
         px = augmented_sigma[UKFState.X]
@@ -102,9 +130,21 @@ class StatePredictor(object):
         return predicted_sigma
 
     def predict_x(self, predicted_sigma):
+        """
+        Calculates the predicted state vector using the predicted sigma points and their associated weight distribution
+        @param predicted_sigma: the predicted state sigma point matrix
+        @return: the predicted state vector in the shape N_X
+        """
         return np.dot(predicted_sigma, self.WEIGHTS_M)
 
     def predict_P(self, predicted_sigma, predicted_x):
+        """
+        Calculates the predicted covariance matrix using the predicted sigma points and their associated weight
+        distribution.
+        @param predicted_sigma: the predicted state sigma point matrix
+        @param predicted_x: the predicted state vector
+        @return: returns the predicted covariance state matrix in the shape N_X x N_X
+        """
         sub = np.subtract(predicted_sigma.T, predicted_x).T
 
         sub[UKFState.YAW] = angle_diff(predicted_sigma[UKFState.YAW], predicted_x[UKFState.YAW])
@@ -112,6 +152,12 @@ class StatePredictor(object):
         return np.matmul(self.WEIGHTS_C * sub, sub.T)
 
     def process(self, x, P, dt):
+        """
+        Processes the current state and estimates its new positions in dt time
+        @param x: the current state vector
+        @param P: the current covariance matrix
+        @param dt: the elapsed time
+        """
         augmented_sigma = self.compute_augmented_sigma(x, P)
         self.sigma = self.predict_sigma(augmented_sigma, dt)
         self.x = self.predict_x(self.sigma)
