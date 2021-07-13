a2381091482a4009a550b62e5dd6c4b5064102ca
Marius-Juston <Marius.juston@hotmail.fr>
2021-07-12 20:56:11 -0400

    commented fusion ukf

--- src/ukf/fusion_ukf.py
+++ src/ukf/fusion_ukf.py
@@ -7,7 +7,37 @@ from ukf.state_updater import StateUpdater
 
 
 class FusionUKF(object):
+    """
+    A class that implements an Unscented Kalman model following a CTRV motion model
+    """
+
     def __init__(self, sensor_std, speed_noise_std=.9, yaw_rate_noise_std=.6, alpha=1, beta=0, k=None):
+        """
+        Setups the UKF constants such as the sigma point weights
+        @param sensor_std: a dictionary with the sensor noise values. Each point in the dictorary is defined using its
+        DataType id as its key, the number values the state contains (nz) and then a list of all the sensor noise
+        values (std). An example is as follows
+
+        >>> sensor_std = {
+        ...   DataType.UWB: {
+        ...       'std': [1],
+        ...       'nz': 1
+        ...   },
+        ...   DataType.ODOMETRY: {
+        ...     'std': [1, 1, 1, 1, 1, 1],
+        ...     'nz': 6
+        ...    },
+        ... }
+
+        @param speed_noise_std: the acceleration noise parameter
+        @param yaw_rate_noise_std: the yaw rate noise parameter
+        @param alpha: a parameter that influences the weight distribution for the sigma points. Acts more like a scale
+         to the weights
+        @param beta: a different parameter that influences the weight distribution for the sigma points. Acts more like
+         a bias/shift to the weights
+        @param k: a parameter that influences the weight distribution for the sigma points. Acts like the x values of a
+         function. By default it is defined as 3 - augmented state vector size
+        """
         # ODOMETRY: beta=0.3
         # UWB: beta=0.3
 
@@ -68,23 +98,40 @@ class FusionUKF(object):
         self.state_updater = StateUpdater(self.NX, self.N_SIGMA, self.WEIGHTS_M, self.WEIGHTS_C)
 
     def initialize(self, x, initial_p, timestamp):
+        """
+        Initializes the UKF's initial state and covariance vectors
+        @param x: The initial state vector
+        @param initial_p: the initial covariance vector
+        @param timestamp: the initial time
+        """
         self.x[:x.size] = x
         self.P = initial_p
         self.initialized = True
         self.timestamp = timestamp
 
     def update(self, data):
+        """
+        Process the measurement data into the Kalman Filter. If it is the first datapoint and the UKF has not
+        initalized then the datapoint must be an odometry measurement.
+        @param data: the measurement data in the form DataPoint.
+        """
         if self.initialized:
             self.process(data)
         else:
             self.initialize(data.measurement_data, np.eye(self.NX), data.timestamp)
 
     def process(self, data):
+        """
+        Goes through the update and predict step of the Kalman Filter using the measurement data.
+        @param data: the measurement data as a DataPoint
+        """
         dt = (data.timestamp - self.timestamp) / 1e9  # seconds
         # dt = (data.timestamp - self.timestamp)  # seconds
 
-        if dt < 0.0001:
-            dt = 0.0001
+        if dt < 0:
+            print("Error went back in time")
+        elif dt > 1:
+            print("Dt between time periods is way too large")
 
         # STATE PREDICTION
         # get predicted state and covariance of predicted state, predicted sigma points in state space
